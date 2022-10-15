09bce0bd4c4fe2dbd6907049dfccc9128882773e
SergioGuevara <49651732+SergioGuevara@users.noreply.github.com>
2022-10-15 10:15:28 -0600

    Login y recuperacion

--- MPL3B/index.js
+++ MPL3B/index.js
@@ -4,6 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import Login from './Vistas/Login';
 import {name as appName} from './app.json';
+import RecupeContra from './Vistas/RecuperacionContra';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Login);
