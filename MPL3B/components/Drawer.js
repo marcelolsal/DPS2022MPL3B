ec47e2dda6e88e252a58d002783736dccf962a9c
Riverapjj <keko.rai.jr@gmail.com>
2022-10-16 12:20:06 -0600

    Enlazando catalogo examenes y citas

--- MPL3B/components/Drawer.js
+++ MPL3B/components/Drawer.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import Splash from "../views/Splash";
+import CatalogoExamenes from "../views/catalogoExamenes";
+import Citas from "../views/tablaCitas";
 
 const Drawer = createDrawerNavigator();
 
 const Menu = () => {
     return(
         <Drawer.Navigator>
-            <Drawer.Screen name="Test" component={Splash} />
+            <Drawer.Screen name="Index" component={Splash} />
+            <Drawer.Screen name="Examenes" component={CatalogoExamenes} />
+            <Drawer.Screen name="Citas" component={Citas} />
         </Drawer.Navigator>
     );
 }
