4f484937eb6358af545cdc6b8cba3727a62a6a16
mariella1402 <mariiellachiicas14@gmail.com>
2022-10-15 20:44:27 -0600

    2

--- MPL3B/index.js
+++ MPL3B/index.js
@@ -3,8 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Splash from './views/Splash';
 import Login from './Vistas/Login';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Splash);
--- MPL3B/views/Index.js
+++ MPL3B/views/Index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Image, Button, Text } from 'react-native';
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
 import Drawer from '../components/Drawer';
 
 const Index = () => {
