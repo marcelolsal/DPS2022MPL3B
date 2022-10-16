eef3a0bdcaf35ee156406305a88a3345ce46718a
SergioGuevara <49651732+SergioGuevara@users.noreply.github.com>
2022-10-16 11:31:31 -0600

    Update

--- MPL3B/index.js
+++ MPL3B/index.js
@@ -5,6 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import Login from './Vistas/Login';
+import ImportarContra from './Vistas/RecuperacionContra';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Login);
