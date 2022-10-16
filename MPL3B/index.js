beaaefe0dab568a3b17d83544c4d855f074ea50b
aby23lucia <luciabay23@gmail.com>
2022-10-15 22:45:29 -0600

    actualizado

--- MPL3B/index.js
+++ MPL3B/index.js
@@ -1,9 +1,10 @@
 /**
  * @format
  */
-
+import Perfil from './Vistas/Perfil';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Perfil);
+
