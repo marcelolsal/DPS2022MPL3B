64b2dd0186fed8e782d5e0ba2e3c5a39d0fabbed
Marcelo Leiva <110951842+marcelolsal@users.noreply.github.com>
2022-10-16 21:15:44 -0600

    Index view (#10)
    * feat: Adds main data about test and view
    
    * feat: Adds Appointments views with random data, for Customer and Admin roles
    
    * feat: Updates Index view with basic images

--- MPL3B/views/Index.js
+++ MPL3B/views/Index.js
@@ -1,10 +1,49 @@
-/* eslint-disable prettier/prettier */
-import React from 'react';
-import 'react-native-gesture-handler';
-import Drawer from '../components/Drawer';
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
 
 const Index = () => {
-  return <Drawer />;
-};
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headText}>
+        Anuncios/Promociones
+      </Text>
+      <Image style={styles.img} source={require('../assets/lab-img.jpg')} />
+      <Text style={styles.paragraph}></Text>
+      <Image style={styles.img} source={require('../assets/doctor-img.jpg')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+    height: 450,
+    backgroundColor: '#F5EEF8'
+  },
+  headText: {
+    margin: 24,
+    marginTop: 0,
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    maxHeight: 50
+  },
+  paragraph: {
+    margin: 24,
+    marginTop: 0,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    maxHeight: 50
+  },
+  img: {
+    resizeMode: 'cover',
+    width: '100%',
+    height: 150
+  }
+});
 
 export default Index;
