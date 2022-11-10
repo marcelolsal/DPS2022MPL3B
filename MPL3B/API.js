a40c9bdcf84f2396c863dbc23237a9f5165623cd
Marcelo Leiva <110951842+marcelolsal@users.noreply.github.com>
2022-11-10 00:51:00 -0600

    feat: Adjusts api access on app

--- MPL3B/API.js
+++ MPL3B/API.js
@@ -2,6 +2,10 @@ import { useState, useEffect } from "react";
 
 const API = "http://192.168.0.6:4000";
 
+
+// User Access to API
+
+
 export const searchUser = (username) => {
     const [res, setRes] = useState('');
     const response = async () => {
@@ -25,27 +29,69 @@ export const searchUser = (username) => {
 };
 
 export const deleteUser = async (id) => {
-    const res = await fetch(`${API}/user/delete${id}`,{
-        method: "GET",
-        headers: {"Accept":"application/json","Content-Type":"application/json"}
-    });
-    return await res.json();
+    const [res, setRes] = useState('');
+    const response = async () => {
+        try {
+            const response = await fetch(`${API}/user/delete${id}`,{
+                method: "GET",
+                headers: {"Accept":"application/json","Content-Type":"application/json"}
+            });
+            const json = await response.json();
+            setRes(json);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+    useEffect(() => {
+        response();
+    }, []);
+    return res;
 };
 
 export const createUser = async (user) => {
-    const res = await fetch(`${API}/user/create`,{
-        method: "POST",
-        headers: {"Accept":"application/json","Content-Type":"application/json"},
-        body: JSON.stringify(user)
-    });
-    return await res.json();
+    const [res, setRes] = useState('');
+    const response = async () => {
+        try {
+            const response = await fetch(`${API}/user/create`,{
+                method: "POST",
+                headers: {"Accept":"application/json","Content-Type":"application/json"},
+                body: JSON.stringify(user)
+            });
+            const json = await response.json();
+            setRes(json);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+    useEffect(() => {
+        response();
+    }, []);
+    return res;
 };
 
 export const updateUser = async (id,data) => {
-    const res = await fetch(`${API}/user/update/${id}`,{
-        method: "POST",
-        headers: {"Accept":"application/json","Content-Type":"application/json"},
-        body: JSON.stringify(user)
-    });
-    return await res.json();
+    const [res, setRes] = useState('');
+    const response = async () => {
+        try {
+            const response = await fetch(`${API}/user/update/${id}`,{
+                method: "POST",
+                headers: {"Accept":"application/json","Content-Type":"application/json"},
+                body: JSON.stringify(user)
+            });
+            const json = await response.json();
+            setRes(json);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+    useEffect(() => {
+        response();
+    }, []);
+    return res;
 };
