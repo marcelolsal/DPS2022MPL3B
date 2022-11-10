e170e6afb49d763ddc8523543126f0dafe6c86da
Marcelo Leiva <110951842+marcelolsal@users.noreply.github.com>
2022-11-10 10:15:56 -0600

    feat: adds routes and functions on api and request

--- API/src/routes/index.js
+++ API/src/routes/index.js
@@ -57,7 +57,7 @@ router.get('/user/delete/:id', async (req, res) => {
     res.send("User deleted");
 });
 
-router.post('/user/update:id', async (req, res) => {
+router.post('/user/update/:id', async (req, res) => {
 
     console.log(req.params.id);
     console.log(req.body);
@@ -65,6 +65,77 @@ router.post('/user/update:id', async (req, res) => {
     await db.collection('users').doc(req.params.id).update(req.body);
 
     res.send({"msg":"User Updated"});
+});
+
+// Citas
+
+router.get('/cita', async (req, res) => {
+    const querySnapshot = await db.collection('citas').get();
+    
+    const citas = querySnapshot.docs.map(doc => ({ id:doc.id, ...doc.data()}));
+
+    console.log(citas)
+
+    res.send(JSON.stringify(citas));
+});
+
+router.get('/cita/user/:id', async (req, res) => {
+    const querySnapshot = await db.collection('citas').get();
+    const citas = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }));
+    var array = [];
+    citas.forEach(cita => {
+        if(cita.idCliente == req.params.id){
+            cita.msg="success";
+            array.push(cita);
+            console.log(cita);
+        }
+    });
+    res.send(JSON.stringify(array));
+});
+
+router.get('/cita/:id', async (req, res) => {
+    const querySnapshot = await db.collection('citas').get();
+    const citas = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }));
+    citas.forEach(cita => {
+        if(cita.id == req.params.id){
+            cita.msg="success";
+            console.log(cita);
+            res.send(JSON.stringify(cita));
+        }
+    });
+    res.send(JSON.stringify(array));
+});
+
+router.post('/cita/create', async (req, res) => {
+
+    const { idCliente, nombreExamen, fechaRegistro, fechaEntregaMuestra } = req.body;
+
+    await db.collection('citas').add({ idCliente, nombreExamen, fechaRegistro, fechaEntregaMuestra });
+
+    res.send({"msg":"Cita Created"});
+});
+
+router.get('/cita/delete/:id', async (req, res) => {
+    
+    await db.collection('citas').doc(req.params.id).delete();
+
+    res.send("Cita deleted");
+});
+
+router.post('/cita/update/:id', async (req, res) => {
+
+    console.log(req.params.id);
+    console.log(req.body);
+
+    await db.collection('citas').doc(req.params.id).update(req.body);
+
+    res.send({"msg":"Cita Updated"});
 })
 
 module.exports = router;
--- MPL3B/API.js
+++ MPL3B/API.js
@@ -32,7 +32,7 @@ export const deleteUser = async (id) => {
     const [res, setRes] = useState('');
     const response = async () => {
         try {
-            const response = await fetch(`${API}/user/delete${id}`,{
+            const response = await fetch(`${API}/user/delete/${id}`,{
                 method: "GET",
                 headers: {"Accept":"application/json","Content-Type":"application/json"}
             });
@@ -80,7 +80,121 @@ export const updateUser = async (id,data) => {
             const response = await fetch(`${API}/user/update/${id}`,{
                 method: "POST",
                 headers: {"Accept":"application/json","Content-Type":"application/json"},
-                body: JSON.stringify(user)
+                body: JSON.stringify(data)
+            });
+            const json = await response.json();
+            setRes(json);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+    useEffect(() => {
+        response();
+    }, []);
+    return res;
+};
+
+// Citas Access to API
+
+export const getCitas = () => {
+    const [res, setRes] = useState('');
+    const response = async () => {
+        try {
+            const response = await fetch(`${API}/cita/`, {
+                method: "GET",
+                headers: { "Accept": "application/json", "Content-Type": "application/json" }
+            });
+            const json = await response.json();
+            setRes(json);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+    useEffect(() => {
+        response();
+    }, []);
+    return res;
+};
+
+export const getCitasUsuario = (id) => {
+    const [res, setRes] = useState('');
+    const response = async () => {
+        try {
+            const response = await fetch(`${API}/cita/user/${id}`, {
+                method: "GET",
+                headers: { "Accept": "application/json", "Content-Type": "application/json" }
+            });
+            const json = await response.json();
+            setRes(json);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+    useEffect(() => {
+        response();
+    }, []);
+    return res;
+};
+
+export const createCita = async (cita) => {
+    const [res, setRes] = useState('');
+    const response = async () => {
+        try {
+            const response = await fetch(`${API}/cita/create`,{
+                method: "POST",
+                headers: {"Accept":"application/json","Content-Type":"application/json"},
+                body: JSON.stringify(cita)
+            });
+            const json = await response.json();
+            setRes(json);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+    useEffect(() => {
+        response();
+    }, []);
+    return res;
+};
+
+export const updateCita = async (id,data) => {
+    const [res, setRes] = useState('');
+    const response = async () => {
+        try {
+            const response = await fetch(`${API}/cita/update/${id}`,{
+                method: "POST",
+                headers: {"Accept":"application/json","Content-Type":"application/json"},
+                body: JSON.stringify(data)
+            });
+            const json = await response.json();
+            setRes(json);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+    useEffect(() => {
+        response();
+    }, []);
+    return res;
+};
+
+export const deleteCita = async (id) => {
+    const [res, setRes] = useState('');
+    const response = async () => {
+        try {
+            const response = await fetch(`${API}/cita/delete/${id}`,{
+                method: "GET",
+                headers: {"Accept":"application/json","Content-Type":"application/json"}
             });
             const json = await response.json();
             setRes(json);
