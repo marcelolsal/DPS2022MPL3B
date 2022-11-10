45de0e3d934636aa3f052c3a041b2de511902d47
Riverapjj <keko.rai.jr@gmail.com>
2022-11-09 21:42:13 -0600

    Rutas coleccion de usuarios

--- API/src/routes/index.js
+++ API/src/routes/index.js
@@ -3,10 +3,58 @@ const { db } = require("../firebase");
 
 const router = Router();
 
-router.get('/', async (req, res) => {
+router.get('/users', async (req, res) => {
     const querySnapshot = await db.collection('users').get();
-    console.log(querySnapshot.docs[0].data());
+    
+    const users = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }))
+
+    console.log(users)
+
     res.send("Hello");
 });
+
+router.post('/new-user', async (req, res) => {
+
+    const { username, name, birthday, cellphone, email} = req.body;
+
+    await db.collection('users').add({
+        username,
+        name,
+        birthday, 
+        email,
+        cellphone
+    });
+
+    res.send("User created");
+});
+
+router.get('/user/:id', async (req, res) => {
+
+    const doc = await db.collection('users').doc(req.params.id).get();
+
+    console.log(doc.data());
+
+    res.send("User");
+});
+
+router.get('/delete-user/:id', async (req, res) => {
+    
+    await db.collection('users').doc(req.params.id).delete();
+
+    res.send("User deleted");
+});
+
+router.post('/update-user/:id', async (req, res) => {
+
+    console.log(req.params.id);
+    console.log(req.body);
+
+    await db.collection('users').doc(req.params.id).update(req.body);
+
+    res.send("User updated")
+})
 
 module.exports = router;
